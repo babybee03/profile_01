285eaf2c9904479e149e80d04724ff0403fa248b
babybee03 <153714246+babybee03@users.noreply.github.com>
2023-12-27 00:14:07 +0900

    12.27

--- js/main_03.js
+++ js/main_03.js
@@ -1,4 +1,3 @@
-/*
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".header_menu");
 
@@ -9,13 +8,3 @@ hamburger.addEventListener("click", function() {
 navMenu.addEventListener("click", function() {
     navMenu.classList.remove("open");
 });
-*/
-
-const hamburger = document.querySelector('.hamburger');
-const headerMenu = document.querySelector('.header_menu')
-hamburger.addEventListener('click', function(){
-    headerMenu.classList.toggle('open');
-})
-headerMenu.addEventListener('click', function(){
-    headerMenu.classList.remove('open');
-})
