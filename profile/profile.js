3141e588d66c461291d1680c24897a39ea164b7a
babybee03 <153714246+babybee03@users.noreply.github.com>
2024-01-07 23:51:56 +0900

    1
    1

---
+++ profile/profile.js
@@ -0,0 +1,6 @@
+var menu_all = document.querySelector(".menu_all");
+var phone_menubar = document.querySelector('.phone_menubar');
+
+addEventListener('click', function(){
+    menu_all.classList.toggle('open');
+})
